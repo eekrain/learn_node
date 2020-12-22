d9ba42170409abb9c4252f4929afd949a56de70b
Ardian Eka Candra <ardianoption10@gmail.com>
2020-12-23 06:13:10 +0700

    setelah refactoring untuk routes

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require("./routes");
 
-  if (url === "/") {
-    res.write("<html><body>");
-    res.write(
-      '<form action="/message" method="POST"><input type="text" name="msg"><button type="submit">send</button>'
-    );
-    res.write("</body></html>");
-    return res.end();
-  }
+console.log(routes.someText);
 
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const msg = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", msg, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html><body><h1>");
-  res.write("Hello from Node!");
-  res.write("</h1></body></html>");
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
