2feb76253d439040ada453a0dec72cef711dd7c5
Ardian Eka Candra <ardianoption10@gmail.com>
2020-12-24 01:11:37 +0700

    selesai part 5 express js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const http = require("http");
+const path = require("path");
 
-const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+
+app.use(shopRoutes);
+
+// Jika url tidak ada yang match, maka 404 error
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404-error.html"));
+});
+
+app.listen(3000);
